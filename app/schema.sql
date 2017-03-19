5a52073eaeb994f2511612e82633ab3075635bb1
Steve Melia <steve.j.melia@gmail.com>
2017-03-19 21:58:42 +0000

    Update schema to stop recreating db

--- app/schema.sql
+++ app/schema.sql
@@ -1,19 +1,16 @@
-drop table if exists rotas;
-create table rotas (
+create table if not exists rotas (
 	id integer primary key autoincrement,
 	uuid text not null,
 	name text not null
 );
 
-drop index if exists rotas_uuid;
-create unique index rotas_uuid on rotas (uuid);
+create unique index if not exists rotas_uuid on rotas (uuid);
 
-drop table if exists entries;
-create table entries (
+create table if not exists entries (
 	id integer primary key autoincrement,
 	rota_id integer,
 	'date' integer not null,
 	'entry' text not null,
 	foreign key(rota_id) references rotas(id)
 );
-create unique index entries_rota_id_date on entries (rota_id, date);
+create unique index if not exists entries_rota_id_date on entries (rota_id, date);
